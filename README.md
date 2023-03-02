62819479f3e98b5d58ad549a2e9633014c6b2d50
Rafael Eduardo Ronchi Filho <rafael.e01@aluno.ifsc.edu.br>
2023-03-02 19:19:03 -0300

    Adicionando o reconhecimento da câmera a subir e descer a tela

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # JSExpertMax Gesture Controller - Semana JS Expert 7.0
 
-Seja bem vindo(a) ao projeto dá Semana Javascript Expert.
+Seja bem vindo(a) ao projeto Semana Javascript Expert.
 
 
 ## Preview
@@ -17,22 +17,17 @@ Seja bem vindo(a) ao projeto dá Semana Javascript Expert.
 
 ## Checklist Features
 - Titles List
-  - [] - Campo para pesquisa não deve travar ao digitar termo de pesquisa
+  - [X] - Campo para pesquisa não deve travar ao digitar termo de pesquisa
   - [] - Deve desenhar mãos na tela e fazer com que elementos em segundo plano  continuem sendo clicáveis  🙌
-  - [] - Deve disparar scroll up quando usar a palma das mãos abertas 🖐
-  - [] - Deve disparar scroll down quando usar a palma das mãos fechadas ✊
+  - [X] - Deve disparar scroll up quando usar a palma das mãos abertas 🖐
+  - [X] - Deve disparar scroll down quando usar a palma das mãos fechadas ✊
   - [] - Deve disparar click no elemento mais próximo quando usar  gesto de pinça 🤏🏻
   - [] - Ao mover elementos na tela, deve disparar evento **:hover** em elementos em contexto
 
 - Video Player
-  - [] - Deve ser possivel de reproduzir ou pausar videos com o piscar de olhos 😁
-  - [] - Todo processamento de Machine Learning deve ser feito via Web worker
+  - [X] - Deve ser possivel de reproduzir ou pausar videos com o piscar de olhos 😁
+  - [X] - Todo processamento de Machine Learning deve ser feito via Web worker
 
-### Desafios
-- [] - Aula 01 - Diferenciar piscada de olhos entre olho direito e esquerdo e atualizar log para mostrar qual olho que piscou.
-- [] - Aula 02 - Reconhecer gestos de mãos individuais e printar no log
-- [] - Aula 03 - A definir
-- [] - Aula 04 - A definir
 
 ### Créditos ao Layout
 - Interface baseada no projeto [Streaming Service](https://codepen.io/Gunnarhawk/pen/vYJEwoM) de [gunnarhawk](https://github.com/Gunnarhawk)
--- second-class-template/pages/titles/src/controllers/handGestureController.js
+++ second-class-template/pages/titles/src/controllers/handGestureController.js
@@ -1,33 +1,62 @@
-export default class HandGestureController{
-    #view
-    #service
-    #camera
-    constructor({ view, service, camera }){
-        this.#service = service
-        this.#view = view
-        this.#camera = camera
+import { prepareRunChecker } from "../../../../lib/shared/util.js"
+
+const { shouldRun: scrollShouldRun } = prepareRunChecker({ timerDelay: 200 })
+export default class HandGestureController {
+  #view
+  #service
+  #camera
+  #lastDirection = {
+    direction: '',
+    y: 0
+  }
+
+  constructor({ view, service, camera }) {
+    this.#service = service
+    this.#view = view
+    this.#camera = camera
+  }
+  async init() {
+    return this.#loop()
+  }
+  #scrollPage(direction) {
+    const pixelsPerScroll = 100
+    if (this.#lastDirection.direction === direction) {
+      this.#lastDirection.y = (
+        direction === 'scroll-down' ?
+          this.#lastDirection.y + pixelsPerScroll :
+          this.#lastDirection.y - pixelsPerScroll
+      )
+    }
+    else {
+      this.#lastDirection.direction = direction
     }
 
-    async init() {
-        return this.#loop()
-    }
+    this.#view.scrollPage(this.#lastDirection.y)
 
-    async estimateHands() {
-        try{
-            const hands = await this.#service.esmimateHands(this.#camera.video)
-            console.log({ hands })
-        } catch (error) {
-            console.error('error', error)
+  }
+  async #estimateHands() {
+    try {
+      const hands = await this.#service.estimateHands(this.#camera.video)
+      for await (const { event, x, y } of this.#service.detectGestures(hands)) {
+        if (event.includes('scroll')) {
+          if(!scrollShouldRun()) continue;
+          this.#scrollPage(event)
         }
-    }
+      }
 
-    async #loop(){
-        await this.#service.initializeDetector() 
-        await this.estimateHands()
+    } catch (error) {
+      console.error('deu ruim**', error)
     }
+  }
 
-    static async initialize(deps) {
-        const controller = new HandGestureController(deps)
-        return controller.init()
-    }
+  async #loop() {
+    await this.#service.initializeDetector()
+    await this.#estimateHands()
+    this.#view.loop(this.#loop.bind(this))
+
+  }
+  static async initialize(deps) {
+    const controller = new HandGestureController(deps)
+    return controller.init()
+  }
 }
