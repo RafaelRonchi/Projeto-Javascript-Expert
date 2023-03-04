314c51a61e50c8676cde55b95c82c42b58a6f14a
Rafael Eduardo Ronchi Filho <rafael.e01@aluno.ifsc.edu.br>
2023-03-04 14:33:23 -0300

    Finalizando o projeto

--- final-class-template/pages/titles/src/factories/handGestureFactory.js
+++ final-class-template/pages/titles/src/factories/handGestureFactory.js
@@ -11,6 +11,8 @@ import HandGestureView from "../views/handGestureView.js"
 import Camera from "../../../../lib/shared/camera.js"
 import { fingerLookupIndexes, gestureStrings, knownGestures } from "../util/util.js"
 
+const styler = new PseudoStyler()
+
 
 const camera = await Camera.init()
 
@@ -20,7 +22,8 @@ const factory = {
     return HandGestureController.initialize({
       camera,
       view: new HandGestureView({
-        fingerLookupIndexes
+        fingerLookupIndexes,
+        styler
       }),
       service: new HandGestureService({
         gestureStrings,
